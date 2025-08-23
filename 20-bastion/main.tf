c7f9b5aaea70016cb82ee499ccf53663c9ee4798
BharathKumarReddy <nbkumar2103@gmail.com>
2025-08-23 19:00:38 +0530

    updated file

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -1,3 +1,33 @@
+# IAM role for bastion (if you already have one, skip this)
+resource "aws_iam_role" "bastion_role" {
+  name = "TerraformAdmin"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# Instance profile that wraps the role
+resource "aws_iam_instance_profile" "bastion_profile" {
+  name = "TerraformAdminProfile"
+  role = aws_iam_role.bastion_role.name
+}
+
+# Attach SSM policy so you can use Session Manager to connect
+resource "aws_iam_role_policy_attachment" "bastion_ssm" {
+  role       = aws_iam_role.bastion_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
 resource "aws_instance" "bastion" {
   ami           = local.ami_id
   instance_type = "t3.micro"
@@ -10,7 +40,7 @@ resource "aws_instance" "bastion" {
     volume_type = "gp3" # or "gp2", depending on your preference
   }
   user_data = file("bastion.sh")
-  iam_instance_profile = "TerraformAdmin"
+  iam_instance_profile = aws_iam_instance_profile.bastion_profile.name
   tags = merge(
     local.common_tags,
     {
