e05491e8772f673661424e91c53c45a3b13dce53
BharathKumarReddy <nbkumar2103@gmail.com>
2025-08-24 11:35:05 +0530

    updated file

--- 60-acm/acm.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_acm_certificate" "bharath2103" {
-  domain_name       = "bharath2103.site"   # change to your domain
-  validation_method = "DNS"
-
-  tags = {
-    Name = "${var.project}-${var.environment}-acm"
-  }
-}
